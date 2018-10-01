dc0fa283b12df114a393b1439b03a1aad4245794
AdityaSuheryana <43383601+AdityaSuheryana@users.noreply.github.com>
2018-10-01 21:52:38 +0700

    PBO3-10117104-Latihan18-Kelipatan

---
+++ PBO3-10117104-Latihan18-Kelipatan/src/pbo3/pkg10117104/latihan18/kelipatan/PBO310117104Latihan18Kelipatan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117104.latihan18.kelipatan;
+
+/**
+ *
+ * @author 
+ * Nama : Aditya Suheryana
+ * Nim  : 10117104
+ * Kelas : IF-3/PBO3
+ * Deskripsi : Menampilkan Kelipatan
+ */
+public class PBO310117104Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        for (double i=3.5;i<=35;i++){
+            System.out.println(i);
+            i=i+2.5;
+    }
+    }
+}
